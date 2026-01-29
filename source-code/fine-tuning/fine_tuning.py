8c412b1e2afbb00e1e5ebb9cde73c739d9741a20
Geert Jan Bex <geertjan.bex@uhasselt.be>
2026-01-29 16:49:32 +0100

    Adapt code for script

--- source-code/fine-tuning/fine_tuning.py
+++ source-code/fine-tuning/fine_tuning.py
@@ -1,23 +1,47 @@
 #!/usr/bin/env python
 
+from datetime import datetime
 import os
 import sys
+from accelerate import Accelerator
 from dotenv import load_dotenv
 from datasets import load_dataset
+from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model
 import torch
+import transformers
 from transformers import (
     AutoTokenizer,
     AutoModelForCausalLM,
     BitsAndBytesConfig
 )
 
-os.environ['BNB_CUDA_VERSION'] = '120'
-load_dotenv()
-
 
 def format_prompt(example):
     return f'### Question: {example["input"]}\n### Answer: {example["output"]}'
 
+def generate_and_tokenize_prompt(prompt):
+    return tokenizer(
+        format_prompt(prompt),
+        max_length=200,
+        padding="max_length"
+    )
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param:.2f}"
+    )
+
+# os.environ['BNB_CUDA_VERSION'] = '120'
+load_dotenv()
 
 train_dataset = load_dataset(
     'json',
@@ -44,8 +68,6 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map="auto"
 )
 
-sys.exit(0)
-
 tokenizer = AutoTokenizer.from_pretrained(
     base_model_id,
     padding_side="left",
@@ -54,22 +76,8 @@ tokenizer = AutoTokenizer.from_pretrained(
 )
 tokenizer.pad_token = tokenizer.eos_token
 
-def generate_and_tokenize_prompt(prompt):
-    return tokenizer(
-        format_prompt(prompt),
-        max_length=200,
-        padding="max_length"
-    )
-
 tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt)
 tokenized_test_dataset = test_dataset.map(generate_and_tokenize_prompt)
-
-
-# ## Base model performance
-
-# To check the performance of the model as is, i.e., without fine-tuning, we can provide a prompt and see the result.
-
-# In[15]:
 
 
 evaluation_prompt = "How to concatenate two dataframes along rows?"
@@ -84,7 +92,6 @@ model_input = evaluation_tokenizer(
     return_tensors="pt"
 ).to("cuda")
 
-
 model.eval()
 with torch.no_grad():
     print(evaluation_tokenizer.decode(
@@ -95,33 +102,10 @@ with torch.no_grad():
         skip_special_tokens=True))
 
 
-# The result is relevant, but it is a question, not an answer to our prompt.
-
-# ## Model architecture
-
-# We can visualize the architecture by simply printing it.
-
-# In[19]:
-
-
 print(model)
-
-
-# ## PEFT: Parameter-Efficient Fine-Tuning
-
-# In[20]:
-
-
-from peft import prepare_model_for_kbit_training
 
 model.gradient_checkpointing_enable()
 kbit_training_model = prepare_model_for_kbit_training(model)
-
-
-# In[21]:
-
-
-from peft import LoraConfig, get_peft_model
 
 config = LoraConfig(
     r=32,
@@ -142,42 +126,13 @@ config = LoraConfig(
 )
 
 peft_model = get_peft_model(kbit_training_model, config)
-
-
-# In[26]:
-
-
-def print_trainable_parameters(model):
-    """
-    Prints the number of trainable parameters in the model.
-    """
-    trainable_params = 0
-    all_param = 0
-    for _, param in model.named_parameters():
-        all_param += param.numel()
-        if param.requires_grad:
-            trainable_params += param.numel()
-    print(
-        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param:.2f}"
-    )
-
-
-# In[27]:
-
-
 print_trainable_parameters(peft_model)
-
-
-# In[28]:
-
-
 print(peft_model)
 
 
 # In[29]:
 
 
-from accelerate import Accelerator
 
 
 # In[30]:
@@ -187,24 +142,8 @@ if torch.cuda.device_count() > 1: # If more than 1 GPU
     peft_model.is_parallelizable = True
     peft_model.model_parallel = True
 
-
-# In[31]:
-
-
 accelerator = Accelerator()
-
-
-# In[32]:
-
-
 accelerated_model = accelerator.prepare_model(peft_model)
-
-
-# In[33]:
-
-
-import transformers
-from datetime import datetime
 
 project = 'pandas_questions'
 base_model_name = "mistral"
@@ -230,7 +169,7 @@ trainer = transformers.Trainer(
         logging_dir="./logs",        # Directory for storing logs
         save_strategy="steps",       # Save the model checkpoint every logging step
         save_steps=25,                # Save checkpoints every 50 steps
-        evaluation_strategy="steps", # Evaluate the model every logging step
+#        evaluation_strategy="steps", # Evaluate the model every logging step
         eval_steps=25,               # Evaluate and save checkpoints every 50 steps
         do_eval=True,                # Perform evaluation at the end of training
     ),
@@ -239,10 +178,3 @@ trainer = transformers.Trainer(
 
 accelerated_model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
 trainer.train()
-
-
-# In[ ]:
-
-
-
-
